c23cc219b4e48d9ada54ce1eb4390fc61c5267a5
Tomáš Silber <silber.tomas@outlook.cz>
2020-06-06 14:18:59 +0200

    Cache statistics

--- swinz-back/src/main/java/cz/osu/swinz/Controllers/StatisticsController.java
+++ swinz-back/src/main/java/cz/osu/swinz/Controllers/StatisticsController.java
@@ -7,11 +7,16 @@ import cz.osu.swinz.home.statistics.RoomStatsGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
+@EnableScheduling
 @RestController
 public class StatisticsController
 {
@@ -20,11 +25,20 @@ public class StatisticsController
     @Autowired
     private RoomRepository roomRepo;
 
+    private RoomStatsGenerator gen;
+    private List<RoomStats> statList;
+
+    @PostConstruct
+    private void init()
+    {
+        gen = new RoomStatsGenerator(ent);
+        reloadCachedStatistics();
+    }
+
     @GetMapping(path="/groups/stats")
     public @ResponseBody ResponseEntity<Iterable<RoomStats>> getRoomStats()
     {
-        RoomStatsGenerator gen = new RoomStatsGenerator(ent);
-        return ResponseEntity.ok(gen.getMonthStats(roomRepo.findAll()));
+        return ResponseEntity.ok(statList);
     }
 
     @GetMapping(path="/groups/{id}/stats/lightWeeks")
@@ -33,8 +47,6 @@ public class StatisticsController
         if(roomRepo.findById(id).isPresent())
         {
             Room room = roomRepo.findById(id).get();
-            RoomStatsGenerator gen = new RoomStatsGenerator(ent);
-
             return ResponseEntity.ok(gen.getAverageLightTwoWeeks(room));
         }
         else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -43,9 +55,13 @@ public class StatisticsController
     @GetMapping(path="/groups/stats/heater")
     public @ResponseBody ResponseEntity<Integer> getHeaterInYear()
     {
-        RoomStatsGenerator gen = new RoomStatsGenerator(ent);
-
         return ResponseEntity.ok(gen.getHeatDaysInYear());
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")//Cron, uložení statistik každý den
+    private void reloadCachedStatistics()
+    {
+        statList = gen.getMonthStats(roomRepo.findAll());
     }
 
     @ExceptionHandler({Exception.class})
